d9de0718e8bde94d071792d3208254e253b3e6ff
javier <ja.quirogab@duocuc.cl>
2026-04-15 03:35:21 -0400

    commit3

--- my-msvc-valle-del-sol/app/page.tsx
+++ my-msvc-valle-del-sol/app/page.tsx
@@ -1,19 +1,11 @@
-import Image from "next/image";
-import "./globals.css";
+import {BrowserRouter , Routes, Route} from "react-router-dom";
+import "./estilos/estilo-pagina.css"
 export default function Home() {
-  const backgroundColor = "#99d20a";
   return (
-    <div>
-      <h1 className="titulo-pagina" style={{backgroundColor}}>Valle del Sol
-        <div>
-          <ul className="lista de botones" style={{backgroundColor}}>
-            <a href="#" className="btn-nav">Reportes</a>
-            <a href="#" className="btn-nav">Geolocalización</a>
-            <a href="#" className="btn-nav">Iniciar Sesion</a>
-            <a href="#" className="btn-nav">Registrarse</a>
-          </ul>
-        </div>
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
