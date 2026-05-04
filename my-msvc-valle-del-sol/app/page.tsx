7f84db6f33d390b69789d0bded1a31a6710ae5d0
javier <ja.quirogab@duocuc.cl>
2026-05-04 16:18:19 -0400

    commit(arreglo de linkeos)

--- my-msvc-valle-del-sol/app/page.tsx
+++ my-msvc-valle-del-sol/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Registrarse from "./registrarse/page";
-import IniciarSesion from "./iniciarsesion/page";
+import IniciarSesion from "./Iniciarsesion/page";
 import Reportes from "./reportes/page";
 import Image from "next/image";
 
