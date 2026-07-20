634ad437426e7eb9d3db4e7cc2d433f20381e2fc
javithox <ja.quirogab@duocuc.cl>
2026-07-20 18:45:19 -0400

    commit(arreglo de linkeo de botones en geolocalizacion)

--- my-msvc-valle-del-sol/app/reportes/page.tsx
+++ my-msvc-valle-del-sol/app/reportes/page.tsx
@@ -68,7 +68,7 @@ export default function Reportes() {
   const enviarReporte = async () => {
     if (!usuario) {
       alert("⚠️ Debes iniciar sesión para reportar un incendio");
-      router.push("/iniciarsesion");
+      router.push("/IniciarSesion");
       return;
     }
 
